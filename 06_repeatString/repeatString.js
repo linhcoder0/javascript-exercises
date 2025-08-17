826bddbf4b617eecbc28465de862ca1da54f0acb
linhcoder0 <114890267+linhcoder0@users.noreply.github.com>
2025-08-16 22:09:08 -0500

    Complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, x) {
+    let arr = [];
+    if(x < 0)
+        {return "ERROR"}
+    for(let i = 0; i < x; i++){
+        arr.push(str)
+        
+    } 
+   return arr.join("");
 };
 
 // Do not edit below this line
