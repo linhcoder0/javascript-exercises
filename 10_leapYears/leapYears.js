aa40ed9a2da4f26d85ff9312624f9cdfe8ba7320
linhcoder0 <114890267+linhcoder0@users.noreply.github.com>
2025-08-17 15:30:44 -0500

    Complete 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(x) {
+    if (x %4 === 0) {
+        if(x % 100 === 0 && x % 400 === 0){
+        return true;
+        }
+        if(x % 100 === 0){
+            return false;
+        }
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
