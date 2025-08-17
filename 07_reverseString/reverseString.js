3cbef0e1ab0029b5bdf8577327fc96d91508cc9c
linhcoder0 <114890267+linhcoder0@users.noreply.github.com>
2025-08-16 22:26:41 -0500

    Complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = [];
+    for(let i = 0; i < str.length; i++){
+        arr[i] = str[i];
+    }
+    arr.reverse();
+    return(arr.join(""));
 };
 
 // Do not edit below this line
